659ec87759cfba42f683cd95d60659b06466cdfe
Fethi-Dev-ID <fsedjai@dev-id.fr>
2025-10-30 16:46:19 +0100

    refactor(roman-numerals): complete TDD cycle 2 — clean implementation Preserve behavior; tests remain green

--- src/convert_numerals_to_roman.ts
+++ src/convert_numerals_to_roman.ts
@@ -17,38 +17,59 @@ export class ConvertNumeralsToRoman {
     },
   ];
 
-  static convert(numerals: number): string {
-    this.checkIfNumberIsInteger(numerals);
-    this.checkIfNumberIsPositive(numerals);
+  static convertFullNumberToRoman(numerals: number): string {
+    this.validateNumber(numerals);
+
+    return this.getRomanNumeralsFromNumber(numerals);
+  }
+
+  private static getRomanNumeralsFromNumber(numerals: number): string {
     const reversed_string_numerals = numerals.toString().split('').reverse().join('');
     let roman_numerals = '';
     for (let i = 0; i < reversed_string_numerals.length; i++) {
-      const digit = parseInt(reversed_string_numerals[i]);
-      if (digit === 0) {
-        continue;
-      }
-      if (digit < 4) {
-        roman_numerals = this.ROMAN_NUMERALS[i][1].repeat(digit) + roman_numerals;
-      } else if (digit === 4) {
-        roman_numerals = this.ROMAN_NUMERALS[i][1] + this.ROMAN_NUMERALS[i][5] + roman_numerals;
-      } else if (digit === 5) {
-        roman_numerals = this.ROMAN_NUMERALS[i][5] + roman_numerals;
-      } else if (digit > 5 && digit < 9) {
-        roman_numerals = this.ROMAN_NUMERALS[i][5] + this.ROMAN_NUMERALS[i][1].repeat(digit - 5) + roman_numerals;
-      } else if (digit === 9) {
-        roman_numerals = this.ROMAN_NUMERALS[i][1] + this.ROMAN_NUMERALS[i][10] + roman_numerals;
-      }
+      const roman_numerals_for_digit = this.getRomanNumeralsForDigit(reversed_string_numerals[i], i);
+      roman_numerals = roman_numerals_for_digit + roman_numerals;
     }
     return roman_numerals;
   }
 
-  private static checkIfNumberIsPositive(numerals: number): void {
-    if (numerals < 0) {
-      throw new Error('Number must be positive');
+  private static getRomanNumeralsForDigit(digit: string, position: number): string {
+    const digit_number = parseInt(digit);
+
+    if (digit_number === 0) {
+      return '';
+    }
+    if (digit_number < 4) {
+      return this.ROMAN_NUMERALS[position][1].repeat(digit_number);
+    }
+    if (digit_number === 4) {
+      return this.ROMAN_NUMERALS[position][1] + this.ROMAN_NUMERALS[position][5];
+    }
+    if (digit_number === 5) {
+      return this.ROMAN_NUMERALS[position][5];
+    }
+    if (digit_number > 5 && digit_number < 9) {
+      return this.ROMAN_NUMERALS[position][5] + this.ROMAN_NUMERALS[position][1].repeat(digit_number - 5);
+    }
+    if (digit_number === 9) {
+      return this.ROMAN_NUMERALS[position][1] + this.ROMAN_NUMERALS[position][10];
+    }
+
+    throw new Error('Invalid digit');
+  }
+
+  private static validateNumber(numerals: number): void {
+    this.validateNumberIsInteger(numerals);
+    this.validateNumberIsBetween0And3999(numerals);
+  }
+
+  private static validateNumberIsBetween0And3999(numerals: number): void {
+    if (numerals < 0 || numerals > 3999) {
+      throw new Error('Number must be between 0 and 3999');
     }
   }
 
-  private static checkIfNumberIsInteger(numerals: number): void {
+  private static validateNumberIsInteger(numerals: number): void {
     if (!Number.isInteger(numerals)) {
       throw new Error('Number must be an integer');
     }
--- tests/roman_numerals.test.ts
+++ tests/roman_numerals.test.ts
@@ -4,12 +4,17 @@ import { ConvertNumeralsToRoman } from '../src/convert_numerals_to_roman';
 describe('Check if the number is valid', () => {
   describe('when the number is not an integer', () => {
     it('should return an error', () => {
-      expect(() => ConvertNumeralsToRoman.convert(1.5)).toThrow('Number must be an integer');
+      expect(() => ConvertNumeralsToRoman.convertFullNumberToRoman(1.5)).toThrow('Number must be an integer');
     });
   });
   describe('when the number is negative', () => {
     it('should return an error', () => {
-      expect(() => ConvertNumeralsToRoman.convert(-1)).toThrow('Number must be positive');
+      expect(() => ConvertNumeralsToRoman.convertFullNumberToRoman(-1)).toThrow('Number must be between 0 and 3999');
+    });
+  });
+  describe('when the number is greater than 3999', () => {
+    it('should return an error', () => {
+      expect(() => ConvertNumeralsToRoman.convertFullNumberToRoman(4000)).toThrow('Number must be between 0 and 3999');
     });
   });
 });
@@ -17,52 +22,52 @@ describe('Check if the number is valid', () => {
 describe('Conversion of single numerals to roman', () => {
   describe('when the number is 0', () => {
     it('should return an empty string', () => {
-      expect(ConvertNumeralsToRoman.convert(0)).toBe('');
+      expect(ConvertNumeralsToRoman.convertFullNumberToRoman(0)).toBe('');
     });
   });
   describe('when the number is 1', () => {
     it('should return I', () => {
-      expect(ConvertNumeralsToRoman.convert(1)).toBe('I');
+      expect(ConvertNumeralsToRoman.convertFullNumberToRoman(1)).toBe('I');
     });
   });
   describe('when the number is 2', () => {
     it('should return II', () => {
-      expect(ConvertNumeralsToRoman.convert(2)).toBe('II');
+      expect(ConvertNumeralsToRoman.convertFullNumberToRoman(2)).toBe('II');
     });
   });
   describe('when the number is 3', () => {
     it('should return III', () => {
-      expect(ConvertNumeralsToRoman.convert(3)).toBe('III');
+      expect(ConvertNumeralsToRoman.convertFullNumberToRoman(3)).toBe('III');
     });
   });
   describe('when the number is 4', () => {
     it('should return IV', () => {
-      expect(ConvertNumeralsToRoman.convert(4)).toBe('IV');
+      expect(ConvertNumeralsToRoman.convertFullNumberToRoman(4)).toBe('IV');
     });
   });
   describe('when the number is 5', () => {
     it('should return V', () => {
-      expect(ConvertNumeralsToRoman.convert(5)).toBe('V');
+      expect(ConvertNumeralsToRoman.convertFullNumberToRoman(5)).toBe('V');
     });
   });
   describe('when the number is 6', () => {
     it('should return VI', () => {
-      expect(ConvertNumeralsToRoman.convert(6)).toBe('VI');
+      expect(ConvertNumeralsToRoman.convertFullNumberToRoman(6)).toBe('VI');
     });
   });
   describe('when the number is 7', () => {
     it('should return VII', () => {
-      expect(ConvertNumeralsToRoman.convert(7)).toBe('VII');
+      expect(ConvertNumeralsToRoman.convertFullNumberToRoman(7)).toBe('VII');
     });
   });
   describe('when the number is 8', () => {
     it('should return VIII', () => {
-      expect(ConvertNumeralsToRoman.convert(8)).toBe('VIII');
+      expect(ConvertNumeralsToRoman.convertFullNumberToRoman(8)).toBe('VIII');
     });
   });
   describe('when the number is 9', () => {
     it('should return IX', () => {
-      expect(ConvertNumeralsToRoman.convert(9)).toBe('IX');
+      expect(ConvertNumeralsToRoman.convertFullNumberToRoman(9)).toBe('IX');
     });
   });
 });
@@ -70,22 +75,22 @@ describe('Conversion of single numerals to roman', () => {
 describe('Conversion of multiple numerals to roman', () => {
   describe('when the number is 14', () => {
     it('should return XIV', () => {
-      expect(ConvertNumeralsToRoman.convert(14)).toBe('XIV');
+      expect(ConvertNumeralsToRoman.convertFullNumberToRoman(14)).toBe('XIV');
     });
   });
   describe('when the number is 35', () => {
     it('should return XXXV', () => {
-      expect(ConvertNumeralsToRoman.convert(35)).toBe('XXXV');
+      expect(ConvertNumeralsToRoman.convertFullNumberToRoman(35)).toBe('XXXV');
     });
   });
   describe('when the number is 49', () => {
     it('should return XLIX', () => {
-      expect(ConvertNumeralsToRoman.convert(49)).toBe('XLIX');
+      expect(ConvertNumeralsToRoman.convertFullNumberToRoman(49)).toBe('XLIX');
     });
   });
   describe('when the number is 99', () => {
     it('should return XCIX', () => {
-      expect(ConvertNumeralsToRoman.convert(99)).toBe('XCIX');
+      expect(ConvertNumeralsToRoman.convertFullNumberToRoman(99)).toBe('XCIX');
     });
   });
 });
